0f2c5026aec339e89db63f0a0c2a2802f78a69b1
Tsknefe <143270111+Tsknefe@users.noreply.github.com>
2025-03-23 23:59:21 +0300

    sumeyra.py

--- sumeyra.py
+++ sumeyra.py
@@ -3,3 +3,4 @@ exchange = ccxt.binance()  # Binance borsasına bağlanıyoruz
 ticker = exchange.fetch_ticker('ETH/BTC')  # BTC/USDT paritesinin verisini alıyoruz
 print(ticker)
 #bu bir deneme blogu satırıdır
+//
